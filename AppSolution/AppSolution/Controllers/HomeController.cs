ac5a0108b039eae8da4bbbfc6f50712ada8bf970
stone <463606175@qq.com>
2016-09-03 10:43:03 +0800

    test1

--- AppSolution/AppSolution/Controllers/HomeController.cs
+++ AppSolution/AppSolution/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AppSolution.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.1";
 
             return View();
         }
